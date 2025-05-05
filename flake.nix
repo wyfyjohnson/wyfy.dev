44e230cf5217f520f7edb7e0dad6605843e74ed7
Wyatt <wyfy.johnson@gmail.com>
2025-05-05 11:56:15 -0700

    adding gitignore and changing the flake around

--- flake.nix
+++ flake.nix
@@ -1,15 +1,16 @@
 {
-  description = "A flake for my wyfy.dev";
+  description = "A flake for my wyfy.dev dev environment";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in
+  {
+    devShells."x86_64-linux".default =
+      import 
   };
 }
